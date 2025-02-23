fa426953eb4aa331573c9ae0e9d0f460ee0aff2b
danielghviniashvili <danielghviniashvili@gmail.com>
2025-02-23 23:14:51 +0400

    second commit

--- day 00/classwork/classwork.py
+++ day 00/classwork/classwork.py
@@ -1,1 +1,13 @@
-print(type(str('20')))
+from turtle import *
+
+print("daniel ghviniashvili")
+
+print(5)
+print("5")
+
+print(5 + int("5"))
+
+
+
+
+print("daniel ghviniashvili aris " + "5" + "wlis")  
---
+++ day 01/classwork/classwork.py
@@ -0,0 +1,20 @@
+name = "danieli"
+
+surname = "ghviniashvili"
+
+
+name = "danieli"
+age = 15
+height = 1.92
+
+knows_programing = True
+is_ugly = False
+
+
+print(name + " " + surname)
+
+print(name = age)
+
+print(name + " " + str(age)
+      
+    
