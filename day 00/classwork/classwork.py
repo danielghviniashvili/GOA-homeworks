b03f5b46c2a39607a0f4aaaec1199299de9f7318
danielghviniashvili <danielghviniashvili@gmail.com>
2025-02-23 23:57:50 +0400

    second commit

--- day 00/classwork/classwork.py
+++ day 00/classwork/classwork.py
@@ -10,4 +10,91 @@ print(5 + int("5"))
 
 
 
-print("daniel ghviniashvili aris " + "5" + "wlis")  
+print("daniel ghviniashvili aris " + "5" + "wlis") 
+
+#we want to draw house
+
+#step 1:   draw a square
+
+
+
+speed(999999999999999999)
+width(7)
+color("brown")
+forward(250) 
+left(90)
+
+forward(300)
+left(90)
+
+forward(250)
+left(90)
+
+forward(300)
+left(90)
+
+forward(100)
+color("brown")
+begin_fill()
+left(90)
+forward(120)     #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(0, 300)
+pendown()
+color("green")
+begin_fill()
+left(140)
+forward(210)
+right(105)
+forward(200)
+end_fill()
+color("yellow")
+penup()
+goto(100, 180)
+pendown()
+begin_fill()
+left(55.2)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+end_fill()
+
+penup()
+goto(-600,450)
+pendown()
+color("yellow")
+begin_fill()
+right(90)
+forward(50)
+
+right(90)
+forward(50)
+
+right(90)
+forward(50)
+
+right(90)
+forward(50)
+end_fill()
+
+
+
+
+
+
+
+
+
+
+exitonclick() 
--- day 02/homework/homework.py
+++ day 02/homework/homework.py
@@ -1,1 +1,25 @@
 #5-5 მაგალითი ყველა არითმეტიკულ მაგალითზე 
+num1 = 5
+num2 = 6
+num3 = 15
+print (num1 + num2) 
+print (num2 * num3)
+print (num1 / num3)
+print (num2 - num1)
+print (num2 // num1)
+str1 = "danieli"
+str2 = "ghviniashvili"
+str3 = "goa"
+float1 = 1.5
+float2 = 2.5
+float3 = 3.5
+print (num1)
+print (num2)
+print (num3)
+print (str1)
+print (str2)
+print (str3)
+print (float1)
+print (float2)
+print (float3)
+print (str1 + str2 + str(num3))
