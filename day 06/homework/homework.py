490b0220595d0a308386c5d77c6d6cc2b05489a9
danielghviniashvili <danielghviniashvili@gmail.com>
2025-03-25 23:26:42 +0400

    7th commit

---
+++ day 06/homework/homework.py
@@ -0,0 +1,25 @@
+sport = input("basketball: ")
+print(sport)
+
+
+
+
+
+#2)მომხმარებელს შეეკითხეთ სახელი, გვარი, ასაკი, საცხოვრებელი ქალაქი,ჰობი შემდეგ კი დაბეჭდეთ გრძელი წინადადება მაგ. შენი სახელია "სახელი და გვარი" შენი ასაკია "ასაკი" და ასე შემდეგ.
+first_name = input("your name: ")
+last_name = input("your lastname: ")
+age = input("your age: ")
+city = input("your living place: ")
+hobby = input("your hobby: ")
+
+#დაბეჭდეთ გრზელი წინადადება
+print("yourname is giorgi ghviniashvili, you are 25 years old, your living place is tbilisi, and your hobby is listening to music")
+
+
+#3)მოხმარებელს შემოატანინეთ რიცხვი და მაგ რიცხვს გამოაკელით 20 და გაამრავლეთ 3 ზე
+number = float(input("enter number: "))
+result = (number - 20) * 3
+print("result")
+
+
+
